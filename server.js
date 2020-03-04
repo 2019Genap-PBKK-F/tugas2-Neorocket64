8d607463420b9e1ef72af1573b3ada7b1ac0ad36
Neorocket64 <neorocket64@gmail.com>
2020-03-04 11:11:36 +0700

    add connect to mssql

--- server.js
+++ server.js
@@ -1,16 +1,43 @@
 /*server.js*/
-
-const http = require('http');
-
-const hostname = '10.199.14.46';
+const hostname = 'localhost';
 const port = 8024;
 
-const server = http.createServer(function(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\nThis isn\'t Nadhif');
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+
+  var sql = require("mssql");
+
+  // config for your database
+  var config = {
+    user: 'su',
+    password: 'SaSa1212',
+    server: '10.199.13.253',
+    database: 'nrp05111740000078'
+  };
+
+  // connect to your database
+  sql.connect(config, function (err) {
+
+    if (err) console.log(err);
+
+    // create Request object
+    var request = new sql.Request();
+
+    // query to the database and get the records
+    request.query('select *', function (err, recordset) {
+
+      if (err) console.log(err)
+
+      // send records as a response
+      res.send(recordset);
+
+    });
+  });
 });
 
-server.listen(port, hostname, function() {
-  console.log('Server running at http://'+ hostname + ':' + port + '/');
+
+var server = app.listen(port, hostname, function () {
+  console.log('Server running at http://' + hostname + ':' + port + '/');
 });
